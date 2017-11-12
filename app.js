0ec70a30b0849c624d3ab92d3a467f6a6e5fca92
CHAMI <zakarya.chami@gmail.com>
2017-11-12 10:29:21 +0100

    second commit

--- app.js
+++ app.js
@@ -7,15 +7,13 @@ angular.module('LunchCheck', [])
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
 
-  $scope.counter = 0;
-  $scope.texte = "cons";
+  $scope.texte = " ";
   $scope.sayMessage ="";
 
-  $scope.upCounter = function(){
+  $scope.wordsCounter = function(){
 
   	$scope.result = $scope.texte.split(',');
-  	//$scope.sayMessage = $scope.result;
-  	if ($scope.result.length <4){
+  	if ($scope.result.length < 4){
   		$scope.sayMessage = "Enjoy !";
   	} else{
   		$scope.sayMessage = "Too much !";
